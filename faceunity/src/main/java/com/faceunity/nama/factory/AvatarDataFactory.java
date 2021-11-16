3eb45f6cf51b4a7a3ffa247abca3a3fd02f7b5a5
shunzhen <shunzhenxu@faceunity.com>
2021-11-16 14:26:31 +0800

    算法demo8.0.1上线

--- faceunity/src/main/java/com/faceunity/nama/factory/AvatarDataFactory.java
+++ faceunity/src/main/java/com/faceunity/nama/factory/AvatarDataFactory.java
@@ -1,12 +1,12 @@
 package com.faceunity.nama.factory;
 
-import com.faceunity.core.avatar.model.PTAAvatar;
-import com.faceunity.core.avatar.model.PTAScene;
-import com.faceunity.core.avatar.scene.SceneHumanProcessor;
+import com.faceunity.core.avatar.model.Avatar;
+import com.faceunity.core.avatar.model.Scene;
+import com.faceunity.core.avatar.scene.ProcessorConfig;
 import com.faceunity.core.entity.FUBundleData;
 import com.faceunity.core.entity.FUCoordinate3DData;
-import com.faceunity.core.enumeration.FUAITypeEnum;
 import com.faceunity.core.faceunity.FURenderKit;
+import com.faceunity.core.faceunity.FUSceneKit;
 import com.faceunity.core.model.antialiasing.Antialiasing;
 import com.faceunity.nama.source.AvatarSource;
 
@@ -25,12 +25,12 @@ public class AvatarDataFactory {
     public final Antialiasing antialiasing;
 
     /* 驱动类型是否为全身  */
-    private Boolean isHumanTrackSceneFull;
+    public Boolean isHumanTrackSceneFull;
 
     /* 场景  */
-    private PTAScene sceneModel;
+    private Scene sceneModel;
     /* 男孩对象  */
-    private PTAAvatar boyAvatarModel;
+    private Avatar boyAvatarModel;
 
 
     public AvatarDataFactory(boolean isFull) {
@@ -41,39 +41,32 @@ public class AvatarDataFactory {
     }
 
     /**
-     * 获取当前驱动类型
-     *
-     * @return
-     */
-    public boolean isHumanTrackSceneFull() {
-        return isHumanTrackSceneFull;
-    }
-
-    /**
      * 设置当前驱动类型
      *
-     * @param isFull
      */
-    public void setHumanTrackSceneFull(boolean isFull) {
-        isHumanTrackSceneFull = isFull;
-        sceneModel.getMSceneHumanProcessor().setTrackScene(isFull ? SceneHumanProcessor.TrackScene.SceneFull : SceneHumanProcessor.TrackScene.SceneHalf);
-        if (isFull) {
-            boyAvatarModel.getMAvatarTransForm().setPosition(new FUCoordinate3DData(0.0, 58.14, -618.94));
+    public void setHumanTrackSceneFull() {
+        sceneModel.processorConfig.setTrackScene(isHumanTrackSceneFull ? ProcessorConfig.TrackScene.SceneFull : ProcessorConfig.TrackScene.SceneHalf);
+        if (isHumanTrackSceneFull) {
+            boyAvatarModel.transForm.setPosition(new FUCoordinate3DData(0.0, 58.14, -618.94));
         } else {
-            boyAvatarModel.getMAvatarTransForm().setPosition(new FUCoordinate3DData(0.0, 11.76, -183.89));
+            boyAvatarModel.transForm.setPosition(new FUCoordinate3DData(0.0, 11.76, -183.89));
         }
     }
 
     public void bindCurrentRenderer() {
         mFURenderKit.getFUAIController().setMaxFaces(1);
         mFURenderKit.setAntialiasing(antialiasing);
-        mFURenderKit.getAvatarContainer().addScene(sceneModel);
-        setHumanTrackSceneFull(isHumanTrackSceneFull);
+        FUSceneKit.getInstance().addScene(sceneModel);
+        FUSceneKit.getInstance().setCurrentScene(sceneModel);
+        setHumanTrackSceneFull();
     }
 
     public void removeScene() {
         mFURenderKit.getFUAIController().setMaxFaces(4);
-        mFURenderKit.getAvatarContainer().removeScene(sceneModel);
-        mFURenderKit.getAvatarContainer().release();
+        FUSceneKit.getInstance().removeScene(sceneModel);
+    }
+
+    public void release() {
+        FUSceneKit.getInstance().release();
     }
 }
